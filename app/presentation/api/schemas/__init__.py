e6b9a071aa8db03e1d205ded9f032a27a48c052f
aleon <azaretov123@gmail.com>
2026-06-14 23:20:14 +0300

    feat: add control endpoints

--- app/presentation/api/schemas/__init__.py
+++ app/presentation/api/schemas/__init__.py
@@ -7,7 +7,21 @@ from app.presentation.api.schemas.content import (
     ModuleStructureResponse,
     SectionStructureResponse,
 )
+from app.presentation.api.schemas.courses import CreateCourseRequest, \
+    UpdateCourseRequest
 from app.presentation.api.schemas.errors import ErrorResponse
+from app.presentation.api.schemas.lectures import CreateLectureRequest, \
+    UpdateLectureRequest
+from app.presentation.api.schemas.modules import (
+    CreateModuleRequest,
+    ModuleResponse,
+    UpdateModuleRequest,
+)
+from app.presentation.api.schemas.sections import (
+    CreateSectionRequest,
+    SectionResponse,
+    UpdateSectionRequest,
+)
 
 __all__ = [
     "CourseListItemResponse",
@@ -17,5 +31,15 @@ __all__ = [
     "LectureStructureResponse",
     "ModuleStructureResponse",
     "SectionStructureResponse",
+    "CreateCourseRequest",
+    "UpdateCourseRequest",
+    "CreateModuleRequest",
+    "UpdateModuleRequest",
+    "ModuleResponse",
+    "CreateSectionRequest",
+    "UpdateSectionRequest",
+    "SectionResponse",
+    "CreateLectureRequest",
+    "UpdateLectureRequest",
     "ErrorResponse",
 ]
